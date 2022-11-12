8ba59885d3d4fba043090fe263a480168396e0fd
kouphasi <phasi.ok.phasi@gmail.com>
2022-11-12 22:24:45 +0900

    allow post

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
+
+@app.route("/api/settime", methods=["POST"])
+def getTime():
+    if request.method == "POST":
+        print(request.json)
+        return jsonify({"response":"response"})
 
 if __name__ == '__main__':
     app.run(debug=True)
